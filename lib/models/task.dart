0be4b115cb3e9cc1f5a4d6b4f5ed5308d825869f
Bartosz <bartekj1221@wp.pl>
2021-09-02 19:19:39 +0200

    Fixed error which caused fromMap to not fully execute

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -10,12 +10,23 @@ class Task {
 
   const Task({this.id, required this.title, this.description, this.subtasks});
 
-  factory Task.fromMap(Map<String, dynamic> json) => Task(
-    id: json['id'] as int?,
-    title: json['title'] as String,
-    description: json['description'] as String?,
-    subtasks: jsonDecode(json['subtasks'] as String) as List<Subtask>?,
-  );
+  factory Task.fromMap(Map<String, dynamic> json) {
+    // jsonDecode(json['subtasks']) returns an array of maps with Subtask fields
+    final jsonSubtasks = jsonDecode(json['subtasks'] as String);
+    final List<Subtask> subtasksList = [];
+    for (final jsonMap in jsonSubtasks){
+      // we convert current map to a Subtask object and add it to a list
+      final subtask = Subtask.fromJson(jsonMap as Map<String, dynamic>);
+      subtasksList.add(subtask);
+    }
+    // we pass obtained subtasks list to the Task constructor as well as other fields
+    return Task(
+      id: json['id'] as int?,
+      title: json['title'] as String,
+      description: json['description'] as String?,
+      subtasks: subtasksList,
+    );
+  }
 
   Map<String, dynamic> toMap() {
     final String jsonSubtasks = jsonEncode(subtasks);
