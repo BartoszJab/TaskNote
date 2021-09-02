0dc6f500eaa780cf58ed225737aae975cb31fd81
Bartosz <bartekj1221@wp.pl>
2021-09-02 19:16:54 +0200

    Created toJson method inside Subtask class to parse object to json format; Created named constructor that allows to create the Subtask class from json format

--- lib/models/subtask.dart
+++ lib/models/subtask.dart
@@ -1,11 +1,12 @@
 class Subtask {
-  final String subtaskText;
-  bool isChecked;
+  String? subtaskText;
+  bool? isChecked;
 
   Subtask({required this.subtaskText, required this.isChecked});
 
-  Map toJson() => {
-    'subtaskText': subtaskText,
-    'isChecked': isChecked
-  };
+  Map toJson() => {'subtaskText': subtaskText, 'isChecked': isChecked};
+
+  Subtask.fromJson(Map<String, dynamic> json) :
+      subtaskText = json['subtaskText'] as String?,
+      isChecked = json['isChecked'] as bool?;
 }
