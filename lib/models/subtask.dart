262953c3a1108fa44101f359f6847864d2408a33
Bartosz <bartekj1221@wp.pl>
2021-09-02 14:18:23 +0200

    Added sqlite dependency; Created task methods to convert data to/from json; Created subtask method to convert data to json format

--- lib/models/subtask.dart
+++ lib/models/subtask.dart
@@ -3,4 +3,9 @@ class Subtask {
   bool isChecked;
 
   Subtask({required this.subtaskText, required this.isChecked});
+
+  Map toJson() => {
+    'subtaskText': subtaskText,
+    'isChecked': isChecked
+  };
 }
--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,11 +1,29 @@
+import 'dart:convert';
+
 import 'package:daily_helper/models/subtask.dart';
 
 class Task {
   final int? id;
-  final String task;
+  final String title;
   final String? description;
   final List<Subtask>? subtasks;
 
-  const Task({this.id, required this.task, this.description, this.subtasks});
+  const Task({this.id, required this.title, this.description, this.subtasks});
 
+  factory Task.fromMap(Map<String, dynamic> json) => Task(
+    id: json['id'] as int?,
+    title: json['title'] as String,
+    description: json['description'] as String?,
+    subtasks: jsonDecode(json['subtasks'] as String) as List<Subtask>?,
+  );
+
+  Map<String, dynamic> toMap() {
+    final String jsonSubtasks = jsonEncode(subtasks);
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'subtasks': jsonSubtasks,
+    };
+  }
 }
